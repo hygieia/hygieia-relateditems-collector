05881f14e0e90aa1d1a53ce0b7ec7b6fa959c5b0
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-09-04 08:57:01 -0400

    Related items collector

--- src/main/java/com/capitalone/dashboard/collector/RelatedItemSettings.java
+++ src/main/java/com/capitalone/dashboard/collector/RelatedItemSettings.java
@@ -2,6 +2,7 @@ package com.capitalone.dashboard.collector;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 /**
@@ -13,6 +14,14 @@ public class RelatedItemSettings {
 
     private String cron;
     private List<String> subscribers;
+    private String clientId;
+    private String clientSecret;
+    long offSet;
+    private String subscriberName;
+    private String tokenUrl;
+    private String accept;
+    private List<String> exclusions;
+
 
     public String getCron() {
         return cron;
@@ -29,6 +38,63 @@ public class RelatedItemSettings {
     public void setSubscribers(List<String> subscribers) {
         this.subscribers = subscribers;
     }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public long getOffSet() {
+        return offSet;
+    }
+
+    public void setOffSet(long offSet) {
+        this.offSet = offSet;
+    }
+
+    public String getSubscriberName() {
+        return subscriberName;
+    }
+
+    public void setSubscriberName(String subscriberName) {
+        this.subscriberName = subscriberName;
+    }
+
+    public String getTokenUrl() {
+        return tokenUrl;
+    }
+
+    public void setTokenUrl(String tokenUrl) {
+        this.tokenUrl = tokenUrl;
+    }
+
+    public String getAccept() {
+        return accept;
+    }
+
+    public void setAccept(String accept) {
+        this.accept = accept;
+    }
+
+    public List<String> getExclusions() {
+        return exclusions;
+    }
+
+    public void setExclusions(List<String> exclusions) {
+        this.exclusions = exclusions;
+    }
+
 
 }
 
--- src/main/java/com/capitalone/dashboard/collector/RelatedItemsCollectorTask.java
+++ src/main/java/com/capitalone/dashboard/collector/RelatedItemsCollectorTask.java
@@ -1,18 +1,25 @@
 package com.capitalone.dashboard.collector;
-import com.capitalone.dashboard.model.Dashboard;
-import com.capitalone.dashboard.model.DashboardType;
 
-import com.capitalone.dashboard.repository.DashboardRepository;
+import com.capitalone.dashboard.client.RelatedItemsClient;
+import com.capitalone.dashboard.model.AutoDiscoverCollectorItem;
+import com.capitalone.dashboard.model.AutoDiscovery;
+import com.capitalone.dashboard.model.Collector;
 import com.capitalone.dashboard.repository.BaseCollectorRepository;
-import com.capitalone.dashboard.repository.ComponentRepository;
 import com.capitalone.dashboard.repository.CollectorItemRepository;
+import com.capitalone.dashboard.repository.ComponentRepository;
+import com.capitalone.dashboard.repository.DashboardRepository;
 import com.capitalone.dashboard.repository.RelatedCollectorItemRepository;
 import com.capitalone.dashboard.repository.RelatedItemCollectorRepository;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * <h1>RelatedItemsCollectorTask</h1>
@@ -30,29 +37,39 @@ public class RelatedItemsCollectorTask extends CollectorTask<RelatedItemsCollect
     private RelatedItemCollectorRepository relatedItemCollectorRepository;
     private RelatedItemSettings settings;
     private static final String COLLECTOR_NAME = "RelatedItems Collector";
+    private RelatedItemsClient relatedItemsClient;
 
     @Autowired
     public RelatedItemsCollectorTask(TaskScheduler taskScheduler, DashboardRepository dashboardRepository,
-                                        ComponentRepository componentRepository,
-                                        CollectorItemRepository collectorItemRepository,
-                                        RelatedCollectorItemRepository relatedCollectorItemRepository,
-                                     RelatedItemCollectorRepository relatedItemCollectorRepository) {
-        super(taskScheduler, COLLECTOR_NAME);
+                                     ComponentRepository componentRepository,
+                                     CollectorItemRepository collectorItemRepository,
+                                     RelatedCollectorItemRepository relatedCollectorItemRepository,
+                                     RelatedItemCollectorRepository relatedItemCollectorRepository,
+                                     RelatedItemsClient relatedItemsClient,
+                                     RelatedItemSettings settings) {
+        super(taskScheduler, "RelatedItemsCollector");
         this.dashboardRepository = dashboardRepository;
         this.componentRepository = componentRepository;
         this.collectorItemRepository = collectorItemRepository;
         this.relatedCollectorItemRepository = relatedCollectorItemRepository;
         this.relatedItemCollectorRepository = relatedItemCollectorRepository;
         this.settings = settings;
+        this.relatedItemsClient = relatedItemsClient;
     }
 
     @Override
     public void collect(RelatedItemsCollector collector) {
         LOGGER.info("RelatedItem Collector");
-        Iterable<Dashboard> dashboards = dashboardRepository.findAllByType(DashboardType.Team);
-
-        LOGGER.info("RelatedItems Collector executed successfully");
-        }
+        logBanner("Collecting related items");
+        long beginTime = getLastUpdated(collector);
+        long endTime = System.currentTimeMillis();
+        Map<ObjectId, List<AutoDiscoverCollectorItem>> dashboardsGrouped = relatedItemsClient.collectAllRelatedCollectorItems(beginTime, endTime);
+        log("Fetched dashboards count :", endTime, dashboardsGrouped.size());
+        List<AutoDiscovery> autoDiscoveries = relatedItemsClient.processRelatedCollectorItems(dashboardsGrouped);
+        log("Fetched auto-discoveries for dashboards :", endTime, autoDiscoveries.size());
+        List<AutoDiscovery> updated = relatedItemsClient.processAutoDiscoveryBatch(autoDiscoveries);
+        log("RelatedItems Collector executed successfully--- requests sent to subscriber", endTime, updated.size());
+    }
 
     @Override
     public RelatedItemsCollector getCollector() {
@@ -72,4 +89,11 @@ public class RelatedItemsCollectorTask extends CollectorTask<RelatedItemsCollect
         return this.settings.getCron();
     }
 
+    private long getLastUpdated(Collector collector) {
+        if (!Objects.isNull(collector.getLastExecuted()) && collector.getLastExecuted() != 0) {
+            return collector.getLastExecuted();
+        } else {
+            return System.currentTimeMillis() - settings.getOffSet();
+        }
+    }
 }
